fc3dd1af58a731532d3fe76dd7c28e7dc35da2ce
BEUMGOO KIM <captain_kbg@naver.com>
2022-02-15 00:13:20 +0900

    dnd todo setting

--- old_todo/CreateToDo.tsx
+++ old_todo/CreateToDo.tsx
@@ -1,17 +1,20 @@
 import { useForm } from "react-hook-form";
-import { useSetRecoilState } from "recoil";
-import { toDoState } from "../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
 
 interface IForm {
   toDo: string;
 }
 
 function CreateToDo() {
+  //atomで設定した値、Propsで渡さなくてもRerenderされる。
   const setToDos = useSetRecoilState(toDoState);
+  const category = useRecoilValue(categoryState);
+
   const { register, handleSubmit, setValue } = useForm<IForm>();
   const handleValid = ({ toDo }: IForm) => {
     setToDos((oldToDos) => [
-      { text: toDo, id: Date.now(), category: "TO_DO" },
+      { text: toDo, id: Date.now(), category },
       ...oldToDos,
     ]);
     setValue("toDo", "");
